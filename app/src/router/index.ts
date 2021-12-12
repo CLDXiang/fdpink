4c2a616f84bf4953da612b2a6f64c8bb30182486
CLDXiang <417198670@qq.com>
2021-12-12 15:46:10 +0800

    feat(app): 准备好页面路由

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,11 +1,43 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomePage from '@/views/HomePage';
+import ConfigPage from '@/views/ConfigPage';
+import CoursePage from '@/views/CoursePage';
+import LoginPage from '@/views/LoginPage';
+import RegisterPage from '@/views/RegisterPage';
+import SearchPage from '@/views/SearchPage';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'HomePage',
     component: HomePage,
+  },
+  {
+    path: '/config',
+    name: 'ConfigPage',
+    components: ConfigPage,
+  },
+  {
+    path: '/course/:id',
+    name: 'CoursePage',
+    component: CoursePage,
+    props: (route) => ({ courseId: route.params.id }),
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage,
+  },
+  {
+    path: './register',
+    name: 'RegisterPage',
+    component: RegisterPage,
+  },
+  {
+    path: './search',
+    name: 'SearchPage',
+    component: SearchPage,
+    props: (route) => ({ q: route.query.q }),
   },
   // {
   //   path: '/about',
---
+++ app/src/views/RegisterPage/RegisterPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full h-full overflow-y-scroll">
+    <NavBar />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NavBar } from '@/components';
+</script>
