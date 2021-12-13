d590696021ad761a2b233120533f5003dc65606b
ahacad <ahacad@126.com>
2021-12-13 23:28:20 +0800

    feat: introduce basic i18n

--- app/src/components/NavBar/NavBar.vue
+++ app/src/components/NavBar/NavBar.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="sticky top-0 w-full h-12 flex justify-center items-center">
-    <SearchInput class="w-5/6 sm:max-w-2xl" />
+  <div class="flex justify-between items-center">
+    <div class="sticky top-0 w-full h-12 flex justify-center items-center">
+      <SearchInput class="w-5/6 sm:max-w-2xl" />
+    </div>
+    <div id="nav-buttons" class="flex w-40 justify-around">
+      <div>{{ $t('courses') }}</div>
+      <div>{{ $t('feedbacks') }}</div>
+      <div>{{ $t('login') }}</div>
+    </div>
   </div>
 </template>
 
--- app/src/main.ts
+++ app/src/main.ts
@@ -3,5 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './index.css';
+import { createI18n } from 'vue-i18n';
 
-createApp(App).use(store).use(router).mount('#app');
+const messages = {
+  zh: {
+    home: '主页',
+    courses: '课程',
+    feedbacks: '评价',
+    login: '登录',
+  },
+};
+
+const i18n = createI18n({
+  locale: 'zh',
+  fallbackLocale: 'en',
+  messages,
+});
+
+createApp(App).use(store).use(router).use(i18n).mount('#app');
