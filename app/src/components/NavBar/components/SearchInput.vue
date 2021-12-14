7aca24f0ae14113fbcc19e8c5615bd4a9285f723
ahacad <ahacad@126.com>
2021-12-14 17:27:52 +0800

    feat(components): basic list items and search input for navbar

--- app/src/components/NavBar/components/SearchInput.vue
+++ app/src/components/NavBar/components/SearchInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="">
-    <input type="text" />
-  </div>
+  <input
+    id="search"
+    autocomplete="off"
+    class="text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-white font-normal h-9 flex items-center pl-3 text-sm border-gray-300 rounded border shadow mr-8"
+    placeholder="Search"
+  />
 </template>
 
 <script lang="ts" setup></script>
